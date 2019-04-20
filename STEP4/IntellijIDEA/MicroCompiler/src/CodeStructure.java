15d925dabb3f493a620def4703ed1b8e6e3302ba
Alexandr <alex.matveyev88@gmail.com>
2019-04-20 12:10:47 -0600

    Updated

---
+++ STEP4/IntellijIDEA/MicroCompiler/src/CodeStructure.java
@@ -0,0 +1,12 @@
+/**
+ *  Currently unused class
+ */
+
+public class CodeStructure
+{
+    private String blockName = null;
+    private boolean blockStart = false;
+    private String blockCondition = null;
+    private String blockBody = null;
+    private boolean blockEnd = false;
+}
